ca5356f3bfa2ad9620725343f1908a1ea1bab22d
petermattia <pattia@stanford.edu>
2017-09-13 00:17:25 -0700

    DATE

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,7 @@ echo "Building blog"
 JEKYLL_ENV=production jekyll build
 echo "Deploying blog to s3"
 s3_website push
+echo "Committing and pushing to GitHub"
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+git commit -a -m DATE
+git push
