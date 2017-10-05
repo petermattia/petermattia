f7342ef94038ef355044aa66865a2ad24c9073da
petermattia <pattia@stanford.edu>
2017-10-05 12:46:46 -0700

    2017-10-05 12:46:46

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ JEKYLL_ENV=production bundle exec jekyll build
 
 echo "Deploying blog to s3"
 echo '----------------------------'
-s3_website push
+s3_website push --verbose
 
 echo "Committing and pushing to GitHub"
 echo '----------------------------'
